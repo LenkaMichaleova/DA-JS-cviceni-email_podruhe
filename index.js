7e5273ddeb5e6bf50d573ab7163627850e22c6c3
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-04-02 19:38:27 +0200

    vyřešeno

--- index.js
+++ index.js
@@ -2,13 +2,35 @@ const goodbye = (name) => {
   return 'S pozdravem ' + name;
 };
 
+const formalGoodbye = (name) => {
+  return 'S uctivou poklonou… ' + name;
+};
+
+const rudeGoodbye = (name) => {
+  return 'Se měj…' + name;
+};
+
 const fillSubject = (subject) => {
   document.querySelector('.email__subject').textContent = subject;
 };
 
-const fillBody = (body, name) => {
+const fillBody = (body, name, goodbyeFunction) => {
   const bodyElm = document.querySelector('.email__body');
   bodyElm.innerHTML = body;
   const closingElm = document.querySelector('.email__closing');
-  closingElm.textContent = goodbye(name);
+  closingElm.textContent = goodbyeFunction(name);
 };
+
+// ------ ukázka použití 1 --------- //
+// ----------------------------------//
+fillSubject('Obchodní sdělení');
+fillBody(
+  'Kupte mycí prostředek, který vám vytře zrak, se slevou 50 %.',
+  'Jan Čistý',
+  formalGoodbye
+);
+
+// ------ ukázka použití 2 --------- //
+// ----------------------------------//
+fillSubject('Pozvánka na oslavu narozenin');
+fillBody('Zítra oslava. 18:00 ve Starý hospodě.', 'Patrik Veselý', rudeGoodbye);
